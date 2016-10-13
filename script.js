7bb9ac1b51762533ec2bd0c2eb1d2f22c2091588
abrilnastassja13 <abril.saravia13@gmail.com>
2016-10-13 00:07:13 -0300

    ejercicio leccion20

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // tu codigo va aca
+
+function callbackPromedio(a, b, c, callback) {
+	var suma = (a + b + c)/3;
+	return callback (suma);
+}
+
+
+var contador = 1;
+function segundero() {
+	document.getElementById("titulo").innerHTML = "5";
+}
+setInterval(segundero, 1000);
